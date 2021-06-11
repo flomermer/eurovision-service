cdebd056c51c8c1bbeae452dfab53ff73e760973
ShakedZrihen <shaked.zrihen@gmail.com>
2021-06-11 10:03:27 +0300

    Update db

--- consts/index.js
+++ consts/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
-  MLAB_URI: 'mongodb://shaked:Shaked1234@ds119024.mlab.com:19024/eurovision'
-}
+  MLAB_URI:
+    "mongodb+srv://shaked:Shaked1234@eurovision.dkzgv.mongodb.net/eurovision",
+};
